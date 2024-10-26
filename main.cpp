3b9cb48dd64ae93f6518ea8188c54520493db093
Mathys Hymon <mathys.hymon@gmail.com>
2024-10-26 20:54:33 +0200

    [add]  shaders fils

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+
+#include "shaderClass.h"
+#include "VBO.h"
+#include "VAO.h"
+#include "EBO.h"
 
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor; \n"
-"void main \n"
-"{ \n"
-"     FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"}\n\0";  
+
 
 int main()
 {
@@ -29,6 +24,21 @@ int main()
 
 	// Define if GLFW use the core profile
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLfloat vertices[] =
+	{
+		//   COORDINATES    /       COLORS       //
+		-0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f, // Lower left corner
+		-0.5f,  0.5f, 0.0f,    0.0f, 1.0f, 0.0f, // Upper left corner
+		 0.5f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f, // Upper right corner
+		 0.5f, -0.5f, 0.0f,    1.0f, 1.0f, 1.0f, // Lower right corner
+	};
+
+	GLuint indices[] =
+	{
+	    0, 2, 1, // Upper triangle
+		0, 3, 2, // Lower triangle
+	};
 
 	// Create the window
 	GLFWwindow* window = glfwCreateWindow(800, 800,"first OpenGL", NULL, NULL);
@@ -50,21 +60,45 @@ int main()
 	// Set the viewport Size
 	glViewport(0, 0, 800, 800);
 
-	// Set the color of the background buffer
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	Shader shaderProgram("default.vert", "default.frag");
 
-	// Clear the front buffer
-	glClear(GL_COLOR_BUFFER_BIT);
+	VAO VAO1;
+	VAO1.Bind();
 
-	// Swap the buffers
-	glfwSwapBuffers(window);
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
+
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	// Texture
+
+	int widthImg, heightImg, numColCh;
+	unsigned char* bytes = stbi_load("Textures/theBlock.png", &widthImg, &heightImg, &numColCh, 0);
 
 	while (!glfwWindowShouldClose(window)) // Main While loop
 	{
-
+		glClearColor(0.07f, 0.13f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		shaderProgram.Activate();
+		glUniform1f(uniID, 0.5f);
+		VAO1.Bind();
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glfwSwapBuffers(window);
 		// Manage all GLFW events
 		glfwPollEvents();
 	}
+
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
 
 	// Destroy the window when closed
 	glfwDestroyWindow(window);
