d685d30068da660c7f158e64dd3da45f5108f07c
Mathys Hymon <mathys.hymon@gmail.com>
2024-10-27 00:40:55 +0200

    [add] 3 dimensions & texture support

--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
+#include<filesystem>
+namespace fs = std::filesystem;
+
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
-
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include"Texture.h"
 #include "shaderClass.h"
 #include "VBO.h"
 #include "VAO.h"
 #include "EBO.h"
 
+
+const unsigned int width = 800;
+const unsigned int height = 800;
+
+// Vertices coordinates
+GLfloat vertices[] =
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
+};
+
+// Indices for vertices order
+GLuint indices[] =
+{
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
+};
 
 
 
@@ -25,23 +55,10 @@ int main()
 	// Define if GLFW use the core profile
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLfloat vertices[] =
-	{
-		//   COORDINATES    /       COLORS       //
-		-0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f, // Lower left corner
-		-0.5f,  0.5f, 0.0f,    0.0f, 1.0f, 0.0f, // Upper left corner
-		 0.5f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f, // Upper right corner
-		 0.5f, -0.5f, 0.0f,    1.0f, 1.0f, 1.0f, // Lower right corner
-	};
 
-	GLuint indices[] =
-	{
-	    0, 2, 1, // Upper triangle
-		0, 3, 2, // Lower triangle
-	};
 
 	// Create the window
-	GLFWwindow* window = glfwCreateWindow(800, 800,"first OpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height,"first OpenGL", NULL, NULL);
 
 	if (window == NULL) // Return error if window dont create correctly
 	{
@@ -58,7 +75,7 @@ int main()
 	gladLoadGL();
 
 	// Set the viewport Size
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 	Shader shaderProgram("default.vert", "default.frag");
 
@@ -68,8 +85,9 @@ int main()
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
 
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 
 	VAO1.Unbind();
 	VBO1.Unbind();
@@ -77,19 +95,56 @@ int main()
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
-	// Texture
+	// Load texture
 
-	int widthImg, heightImg, numColCh;
-	unsigned char* bytes = stbi_load("Textures/theBlock.png", &widthImg, &heightImg, &numColCh, 0);
+	std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
+	std::string texPath = "/FistOpenGL/Resources/";
+
+	Texture theBlock((parentDir + texPath + "theBlock.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	theBlock.texUnit(shaderProgram, "tex0", 0);
+
+	// set mesh rotation
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST); // Enable depth buffer
 
 	while (!glfwWindowShouldClose(window)) // Main While loop
 	{
 		glClearColor(0.07f, 0.13f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear the background color and depth
 		shaderProgram.Activate();
+
+		double crntTime = glfwGetTime();
+
+		if (crntTime - prevTime >= 1 / 60) { // increment mesh rotation
+			rotation += 0.5f;
+			prevTime = crntTime;
+		}
+
+		//Create the matrices
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view  = glm::mat4(1.0f);
+		glm::mat4 proj  = glm::mat4(1.0f);
+
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f)); // apply the rotation to the mesh
+		//apply camera effect and perspective
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+
 		glUniform1f(uniID, 0.5f);
+		theBlock.Bind();
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		// Manage all GLFW events
 		glfwPollEvents();
@@ -98,6 +153,7 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	theBlock.Delete();
 	shaderProgram.Delete();
 
 	// Destroy the window when closed
